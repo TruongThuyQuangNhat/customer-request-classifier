48b70351df86628f1c63beccebf99748beaffab5
Quang Nhat <truongthuyquangnhat1@gmail.com>
2025-07-26 11:46:22 +0700

    update loading

--- src/app/classify-request/classify-request.component.ts
+++ src/app/classify-request/classify-request.component.ts
@@ -9,18 +9,22 @@ import { environment } from 'src/environments/environment';
 })
 export class ClassifyRequestComponent {
   text: string = '';
+  loading = false;
   result: string | null = null;
 
   constructor(private http: HttpClient) {}
 
   classify() {
+    this.loading = true;
     this.http.post<any>(`${environment.apiUrl}/predict`, { text: this.text }).subscribe({
       next: (response) => {
         this.result = response.label;
+        this.loading = false;
       },
       error: (err) => {
         console.error('Lỗi gọi API:', err);
         this.result = 'Lỗi phân loại';
+        this.loading = false;
       },
     });
   }
